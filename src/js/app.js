461f616b9f6d53f2d3310edb892960517d7d8e98
Daniil-Okt <danikoktysyk@gmail.com>
2023-06-14 16:59:02 +0300

    сверстал попап окно

--- src/js/app.js
+++ src/js/app.js
@@ -59,7 +59,7 @@ new BurgerMenu().init();
  * На обертку(враппер) окна добавь класс _overlay-bg
  * На кнопку для закрытия окна добавь класс button-close
  */
-// togglePopupWindows();
+togglePopupWindows();
 
 // =======================================================================================================
 // const tabs = new Tabs('default-tabs', {});
@@ -72,3 +72,105 @@ new BurgerMenu().init();
 // import { useDynamicAdapt } from './modules/dynamicAdapt.js'
 // useDynamicAdapt()
 // =======================================================================================================
+
+const url = 'static/send.php'
+document.addEventListener('DOMContentLoaded', () => {
+  const formPopup = document.getElementById('form-popup')
+  
+  formPopup.addEventListener('submit', formSend)
+
+  // функция обработки формы
+  async function formSend(e) { 
+    e.preventDefault()
+
+    let error = formValidate(formPopup)
+
+    let formData = new FormData(formPopup)
+
+      if (error === 0) {
+      //   отправка полученных данных с формы в файл php
+          fetch(url, {
+              // method: 'POST',
+              // body: formData
+          })
+          .then(response => {
+            if (response.ok) {
+              // Обработка успешной отправки формы
+              console.log('Form was submitted successfully!');
+              formPopup.classList.add('open')
+              formPopup.reset()
+            } else {
+              // Обработка ошибок отправки формы
+              console.log('An error occurred while submitting the form.');
+            }
+          })
+          .catch(error => {
+            console.log('An error occurred while submitting the form:', error);
+          });
+          
+      }
+  }
+  const inputFormRecord = formPopup.querySelectorAll('.form-record__input') 
+  if (inputFormRecord.length > 0) {
+    inputFormRecord.forEach(input => {
+      input.addEventListener('input', function() {
+        if (input.value.length > 0) {
+              formRemoveError(input);
+        }
+    });
+    });
+  }
+  function formValidate(popup) {
+    let error = 0;
+    // технический класс который нужно добавиь на те инпуты которые нужно проверять
+    let formReq = formPopup.querySelectorAll('._req')
+    for (let index = 0; index < formReq.length; index++) {
+        const input = formReq[index];
+        //вначале убираем класс error с инпута
+        formRemoveError(input)
+
+        //проверка инпуста с email, нужно добавить класс к инпуту
+
+        if (input.classList.contains('_email')) {
+            //проверка или email соответствует
+            if (emailTest(input)) {
+                //если проверка не прохожит до добавляетм класс ошибки
+                formAddError(input)
+                error++
+            }
+                //проверяем или является чек боксом
+                //проверка что это чекбок       проверка что этот чекбокс не влючен
+            } else if(input.getAttribute("type") === "checkbox" && input.checked === false) {
+                //добавляем к нему класс ошибки 
+                formAddError(input)
+                error++
+            } else if(input.getAttribute("type") === "tel") {
+              //добавляем к нему класс ошибки 
+              if(telTest(input)) {
+                formAddError(input)
+                error++
+              }
+            }
+             else if (input.value === '' && input.value < 2) {
+            //проверка всех остальных инпутов заполнены они или нет
+                formAddError(input)
+                error++
+            }
+        }
+        return error
+    }
+
+
+//функции добавление и удаление класса ошибки
+function formAddError(input) {
+    input.parentElement.classList.add('_error')
+    input.classList.add('_error')
+}
+function formRemoveError(input) {
+    input.parentElement.classList.remove('_error')
+    input.classList.remove('_error')
+}
+function telTest(input) {
+  return !/^[\d\+][\d\(\)\ -]{4,14}\d$/.test(input. value);
+}
+})
